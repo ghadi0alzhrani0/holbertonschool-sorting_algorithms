a3dbfa2df5c6456eeac80e72ea08ce97d15217a3
areej-alghamdi <areejaa12@gmail.com>
2026-05-08 23:23:14 +0300

    Add selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ *
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+	size_t min;
+	int tmp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
+	}
+}
+
